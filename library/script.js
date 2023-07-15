6a20714dc62118b481256c5ac88c737d9675fb94
asselzhl <asselzhly@gmail.com>
2023-07-15 14:02:57 +0600

    feat: add slider

--- library/script.js
+++ library/script.js
@@ -1,1 +1,27 @@
 'use strict'
+const wrapper = document.querySelector('.slider__wrapper');
+const dots = document.querySelectorAll('.dot');
+
+
+let activeDotnum = 0;
+
+dots.forEach ((dot, i) => {
+    dot.setAttribute('data-num', i);
+
+    dot.addEventListener('click', changeClientWidth);
+
+    function changeClientWidth (e) {
+        let clickedDotNum = e.target.dataset.num;
+        if (clickedDotNum == activeDotnum) {
+            return;
+        } else {
+            let imageWidth = wrapper.firstElementChild.clientWidth;
+            let pixels = (-imageWidth * clickedDotNum) - (clickedDotNum * 25)
+            wrapper.style.transform = 'translateX(' + pixels + 'px)';
+            dots[activeDotnum].classList.remove('active');
+            dots[clickedDotNum].classList.add('active');
+            activeDotnum = clickedDotNum;
+        }
+    }
+});
+
