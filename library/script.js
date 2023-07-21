c32f41bff4fd7c822e5ee0026d8765a52ab5167a
asselzhl <asselzhly@gmail.com>
2023-07-21 20:26:44 +0600

    feat: update files

--- library/script.js
+++ library/script.js
@@ -69,7 +69,6 @@ const dropMenu = document.querySelector('.drop__menu');
 userIcon.addEventListener('click', function (e) {
     dropMenu.classList.toggle('_active');
 });
-
 /*----------------------------------MODAL----------------------------------*/
 const dropLogin = document.getElementById('dropLogin');
 const loginModal = document.getElementById('loginModal');
@@ -87,6 +86,8 @@ window.addEventListener('click', function (e) {
         loginModal.classList.remove('_active');
     } else if (e.target === registerModal) {
         registerModal.classList.remove('_active');
+    } else if (e.target === buyCardModal) {
+        buyCardModal.classList.remove('_active');
     }
 });
 
@@ -96,5 +97,37 @@ for (let closeButton of closeButtons) {
         registerModal.classList.remove('_active');
     })
 }
+const signupButton = document.getElementById('signupButton');
+const loginButton = document.getElementById('loginButton');
+signupButton.addEventListener('click', function (e) {
+    registerModal.classList.add('_active');
+});
+loginButton.addEventListener('click', function (e) {
+    loginModal.classList.add('_active');
+});
+
+/*----------------------------------BUY-CARD__MODAL----------------------------------*/
+const buyButtons = document.querySelectorAll('.card__button');
+const buyCardModal = document.querySelector('.modal__buy-card');
+const buyCardClose = document.querySelector('.buy-card__close');
+for (let buyButton of buyButtons) {
+    buyButton.addEventListener('click', function (e) {
+        buyCardModal.classList.add('_active');
+    })
+};
+buyCardClose.addEventListener('click', function (e) {
+    buyCardModal.classList.remove('_active');
+});
 
 
+
+
+const checkButton = document.querySelector('.form__button');
+const loginTitle = document.querySelector('.login__title');
+const loginText = document.querySelector('.login__text');
+checkButton.addEventListener('submit', e => {
+    e.preventDefault ();
+    
+    loginTitle.innerHTML = 'Visit your profile';
+    loginText.innerHTML = 'With a digital library card you get free access to the Library’s wide array of digital resources including e-books, databases, educational resources, and more.'
+})
